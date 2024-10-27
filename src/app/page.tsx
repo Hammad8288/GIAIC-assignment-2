248ca179a51b8d06af71a7e68db3d9b53837b405
Hammad8288 <159915404+Hammad8288@users.noreply.github.com>
2024-10-27 17:07:21 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Box from "./component/box";
+import Main from "./component/main";
+import Navbar from "./component/Navbar";
+
+export default function App() {
+  return (
+    <div className="home bg-[#252B42] ">
+      <Navbar />
+      <Main />
+      <Box />
+    </div>
+  );
+}
